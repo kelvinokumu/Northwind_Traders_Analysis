98c2b16ebf41171c1f50f41ede86e90193bbf207
kelvinokumu <oumakelvinokumu@gmail.com>
2023-08-17 20:57:14 +0300

    changes

---
+++ sql_scripts/.ipynb_checkpoints/basics-checkpoint.sql
@@ -0,0 +1,31 @@
+-- 1. Retrieve all columns from the "categories" table.
+SELECT *
+FROM categories;
+
+-- 2. Retrieve all columns from the "customers" table and display the first 5 rows.
+SELECT *
+FROM customers
+LIMIT 5;
+
+-- 3. Retrieve all columns from the "employees" table.
+SELECT *
+FROM employees;
+
+-- 4. Retrieve all columns from the "order_details" table and display the first 5 rows.
+SELECT *
+FROM order_details
+LIMIT 5;
+
+-- 5. Retrieve all columns from the "orders" table and display the first 5 rows.
+SELECT *
+FROM orders
+LIMIT 5;
+
+-- 6. Retrieve all columns from the "products" table and display the first 5 rows.
+SELECT *
+FROM products
+LIMIT 5;
+
+-- 7. Retrieve all columns from the "shippers" table.
+SELECT *
+FROM shippers;
